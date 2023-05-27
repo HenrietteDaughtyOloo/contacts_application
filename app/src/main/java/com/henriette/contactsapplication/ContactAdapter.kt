46573e14afe3a56016c08a3f66589a8e2e7858e7
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-05-27 08:07:36 +0300

    corrected the binding

--- app/src/main/java/com/henriette/contactsapplication/ContactAdapter.kt
+++ app/src/main/java/com/henriette/contactsapplication/ContactAdapter.kt
@@ -1,34 +1,29 @@
 package com.henriette.contactsapplication
 
-//class ContactAdapter { var contactList: List<ContactData>:RecyclerView.Adapter<ContactViewHolder>(){
-//
-//}
-////    var tweetList: List<TweetData>):RecyclerView.Adapter<TweetViewHolder>(){
-//}
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.henriette.contactsapplication.databinding.ContactListItemBinding
-
-class ContactRvAdapter(var contactList: List<ContactData>) :
-    RecyclerView.Adapter<ContactViewHolder>() {
+class ContactAdapter (var ContactList:List<ContactData>):RecyclerView.Adapter<ContactViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val binding =
-            ContactListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ContactListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return ContactViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return contactList.size
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        var currentContact=ContactList.get(position)
+        var binding = holder.binding
+//        binding.ivAvatar.text=currentContact.avatar
+        binding.tvDisplayName.text=currentContact.dislayName
+        binding.tvPhoneNumber.text=currentContact.phoneNumber
+        binding.tvEmail.text=currentContact.emailAddress
     }
 
-    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-        val currentContact = contactList.get(position)
-        val binding = holder.binding
-        binding.ivAvator.text = currentContact.avatar
-        binding.tvNameDisplay.text = currentContact.dislayName
-        binding.tvNum.text = currentContact.phoneNumber
-        binding.tvEmail.text = currentContact.emailAddress
+    override fun getItemCount(): Int {
+        return ContactList.size
+
     }
+}
+class ContactViewHolder(var binding: ContactListItemBinding):ViewHolder(binding.root)
--- app/src/main/java/com/henriette/contactsapplication/MainActivity.kt
+++ app/src/main/java/com/henriette/contactsapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.henriette.contactsapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.henriette.contactsapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onResume() {
+        super.onResume()
+        displayContact()
+    }
+    fun displayContact(){
+        val contact1=ContactData("HE","Henriette","0791653445","henriette@gmail.com")
+        val contact2=ContactData("HE","Henriette","0791653445","henriette@gmail.com")
+        val contact3=ContactData("HE","Henriette","0791653445","henriette@gmail.com")
+        val contact4=ContactData("HE","Henriette","0791653445","henriette@gmail.com")
+        val contactList = listOf(contact1,contact2,contact3,contact4)
+        val ContactAdapter=ContactAdapter(contactList)
+        binding.rvContacts.layoutManager=LinearLayoutManager(this)
+        binding.rvContacts.adapter=ContactAdapter
+    }
+
 }
