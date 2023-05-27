d000c132cdfc790bf64ca7dab4a02580cbb68e59
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-05-27 08:54:34 +0300

    Changes added to match parent height

--- app/src/main/java/com/henriette/contactsapplication/MainActivity.kt
+++ app/src/main/java/com/henriette/contactsapplication/MainActivity.kt
@@ -18,11 +18,17 @@ class MainActivity : AppCompatActivity() {
         displayContact()
     }
     fun displayContact(){
-        val contact1=ContactData("HE","Brchicho Mee","0791653445","brchicho@gmail.com")
-        val contact2=ContactData("HE","Trcy ","0791653445","trcy@gmail.com")
-        val contact3=ContactData("HE","Jefferey Jmes","0791653445","jeff@gmail.com")
-        val contact4=ContactData("HE","Lucy Monic","0791653445","monic@gmail.com")
-        val contactList = listOf(contact1,contact2,contact3,contact4)
+        val contact1=ContactData("HE","Barachicho Mee","0791653445","brchicho@gmail.com")
+        val contact2=ContactData("@drawable/barachicha","Trcy ","0706553445","trcy@gmail.com")
+        val contact3=ContactData("HE","Jefferey Jmes","0711153445","jeff@gmail.com")
+        val contact4=ContactData("HE","Lucy Monic","0734553445","monic@gmail.com")
+        val contact5=ContactData("HE","Boaz","07890153445","boaz@gmail.com")
+        val contact6=ContactData("HE","Maina King'ang'i","0711153445","droid@gmail.com")
+        val contact7=ContactData("HE","Jonas","0718953445","jelefoff@gmail.com")
+        val contact8=ContactData("HE","henriquetta","0778153445","henriquetta@gmail.com")
+        val contact9=ContactData("HE","Gredi","0711153445","gredi@gmail.com")
+
+        val contactList = listOf(contact1,contact2,contact3,contact4,contact5,contact6,contact7,contact8,contact9)
         val ContactAdapter=ContactAdapter(contactList)
         binding.rvContacts.layoutManager=LinearLayoutManager(this)
         binding.rvContacts.adapter=ContactAdapter
